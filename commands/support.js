581d4e42adcb1471b7b438ed69dba8ddc0bb6df8
akxsh9 <biswasswapana123@gmail.com>
2025-07-19 21:33:52 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -10,10 +10,7 @@ module.exports = {
     run: async (client, interaction, lang) => {
         try {
             const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
-
+            const githubLink = "https://github.com/akxsh9";
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
                 .setAuthor({
@@ -24,10 +21,7 @@ module.exports = {
                 .setDescription(lang.support.embed.description
                     .replace("{supportServerLink}", supportServerLink)
                     .replace("{githubLink}", githubLink)
-                    .replace("{replitLink}", replitLink)
-                    .replace("{youtubeLink}", youtubeLink)
                 )
-                .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
                 .setTimestamp();
 
             await interaction.reply({ embeds: [embed] });
